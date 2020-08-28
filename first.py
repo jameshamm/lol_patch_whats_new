05bf2ca0cbef9b63d7f99b9770856e69b95cac6c
James Hamm <jameshamm1995@gmail.com>
2020-08-28 20:55:09 +0100

    began diffing

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-from riotwatcher import LolWatcher, ApiError
+from riotwatcher import LolWatcher
 from pathlib import Path
 
 
@@ -14,13 +14,60 @@ def fetch_key(filename):
     return key
 
 
+def is_interesting(key):
+    return key not in ("version", "x", "y")
+
+
+def _keep_interesting(data):
+    if isinstance(data, (str, int, float)):
+        return data
+
+    new_data = dict()
+    for k, v in data.items():
+        # Base case
+        if isinstance(v, (str, int, float)):
+            if is_interesting(k):
+                new_data[k] = v
+        elif isinstance(v, list):
+            if is_interesting(k):
+                new_data[k] = [_keep_interesting(vv) for vv in v]
+        elif isinstance(v, dict):
+            new_data[k] = _keep_interesting(v)
+        else:
+            raise ValueError("Don't understand this")
+
+    return new_data
+
+
+def keep_interesting(original_data):
+    return _keep_interesting(original_data)
+
+
+def champion_differences(lol_watcher, old_version, new_version):
+    dd = lol_watcher.data_dragon
+
+    old_data = dd.champions(old_version)
+    new_data = dd.champions(new_version)
+
+    old_data = keep_interesting(old_data)
+    new_data = keep_interesting(new_data)
+
+    return old_data, new_data
+
+
 def main():
     api_key = fetch_key("riot_api_key.txt")
 
     lol_watcher = LolWatcher(api_key)
-    region = "euw1"
 
-    print(lol_watcher.summoner.by_name(region, 'spyr03'))
+    old, new = champion_differences(lol_watcher, "10.5.1", "10.16.1")
+
+    import json
+    with open("old.json", 'w+') as f:
+        json.dump(old, f, indent=4, sort_keys=True)
+
+    with open("new.json", 'w+') as f:
+        json.dump(new, f, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
